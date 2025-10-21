4bb13ca341822d815526e4930463623bc313a7b6
HenriqueSols <henrique.soares.ribeiro@escola.pr.gov.br>
2025-10-21 10:58:32 -0300

    ok2

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-let titulo = document.querySelector('hi');
-titulo.innerHTML = 'Jogo do número secreto';
+// let titulo = document.querySelector('hi');
+// titulo.innerHTML = 'Jogo do número secreto';
 
 let paragrafo = document.querySelector('p')
 paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
