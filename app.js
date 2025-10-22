1284011856bbfa6088b3bd193549848fd939b54d
HenriqueSols <henrique.soares.ribeiro@escola.pr.gov.br>
2025-10-22 08:17:18 -0300

    ok3

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 // let titulo = document.querySelector('hi');
 // titulo.innerHTML = 'Jogo do número secreto';
 
-let paragrafo = document.querySelector('p')
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+// let paragrafo = document.querySelector('p')
+// paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 
-function verificarChute(){
-    console.log('O botão foi clicado!')
+function exibirTextoNaTela(tag, Texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = Texto;
+}
+
+exibirTextoNaTela('h1','Jogo do número secreto')
+exibirTextoNaTela('p', 'E')
+
+function verificarChute() {
+    console.log('O Botão foi clicado!');
 }
